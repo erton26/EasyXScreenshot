384aed636ade0920474ac44898bdc95d47cf10ff
erton26 <rton26@gmail.com>
2024-07-10 21:34:38 +0900

    Added comment

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
 const handleError = (error) => {
+    /*
+    Error handler
+    */
     console.log(error);
 };
 
 const downloadUri = (uri, filename) => {
+    /*
+    Download the screenshot
+    */
     const link = document.createElement("a");
     link.download = filename;
     link.href = uri;
@@ -11,6 +17,9 @@ const downloadUri = (uri, filename) => {
 };
 
 const captureScreenshot = async(tab, rect) => {
+    /*
+    Capture screenshot of post and download the screenshot
+    */
     try {
         const imageUri = await browser.tabs.captureTab(tab.id, { format: "png", rect: rect });
         const currentDate = new Date().toISOString();
@@ -22,6 +31,9 @@ const captureScreenshot = async(tab, rect) => {
 };
 
 const handleAction = async(tab) => {
+    /*
+    Fetch rect from index.js and capture screenshot according to rect
+    */
     try {
         const rect = await browser.tabs.sendMessage(tab.id, { message: "1" });
         captureScreenshot(tab, rect);
@@ -32,6 +44,10 @@ const handleAction = async(tab) => {
 };
 
 const actionClicked = async() => {
+    /*
+    Fired when addon symbol is clicked.
+    Passes the current opened tab to handleAction.
+    */
     try {
         const response = await browser.tabs.query({
             currentWindow: true,
